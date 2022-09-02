16f30ecd87ed1cd72365d7c8120214478e408ac2
Kazeemakeem <52778248+Kazeemakeem@users.noreply.github.com>
2022-09-02 22:42:21 +0100

    Update textForTesting.jsx

--- src/textForTesting.jsx
+++ src/textForTesting.jsx
@@ -6,7 +6,7 @@ const textForTesting = () => {
       <p>
         
 
-Welcome to Markdown
+# Welcome to Markdown
 
 Markdown is a lightweight markup language that you can use to add formatting elements to plaintext text documents.
 
