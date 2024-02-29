076f7245f1978f2c0a37f59b38962aa61a5059b1
Kazeemakeem <52778248+Kazeemakeem@users.noreply.github.com>
2024-02-29 11:03:18 +0100

    state-lag-fix

--- src/features/documents/DocumentSliceView.tsx
+++ src/features/documents/DocumentSliceView.tsx
@@ -1,121 +1,148 @@
-import React, { useRef, useState, createContext, useEffect} from 'react'
-import { useAppDispatch, useAppSelector } from '../../app/hooks'
-import { addNewDoc } from './documentsSlice'
-import './document.css'
-import { documentIcon, toggleLeftIcon, toggleRightIcon } from '../icons/Icons'
-import { updateTab, closeButtonClicked } from '../header/headerSlice'
-import { updateMarkdown } from '../dataArrays/dataArraysSlice'
-import { Moon, Sun } from "phosphor-react"
-import { activateDarkMode } from '../theme/themeSlice'
-import { updateMarkup } from '../dataArrays/dataArraysSlice'
-import { convertContent } from '../../utils/convertContent'
+import React, { useRef, useState, createContext, useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { addNewDoc } from "./documentsSlice";
+import "./document.css";
+import { documentIcon, toggleLeftIcon, toggleRightIcon } from "../icons/Icons";
+import { updateTab, closeButtonClicked } from "../header/headerSlice";
+import { updateMarkdown } from "../dataArrays/dataArraysSlice";
+import { Moon, Sun } from "phosphor-react";
+import { activateDarkMode } from "../theme/themeSlice";
+import { updateMarkup } from "../dataArrays/dataArraysSlice";
+import { convertContent } from "../../utils/convertContent";
 
 //  export let currentDocNameTab:string = ''
 //  export let currentDocMarkdown: string[] = []
 
 const DocumentSliceView = () => {
-  const [value, setValue] = useState('')
-  const [hideNewName, setHideNewName] = useState(true)
-  const dispatch = useAppDispatch()
-  const documents = useAppSelector((state) => state.documents)
-  const newDocName: string = value
-  const reformedMarkdownArr = useAppSelector(state => state.dataArrays.reformedMarkdownArr)
+  const [value, setValue] = useState("");
+  const [hideNewName, setHideNewName] = useState(true);
+  const dispatch = useAppDispatch();
+  const documents = useAppSelector((state) => state.documents);
+  const newDocName: string = value;
+  const reformedMarkdownArr = useAppSelector(
+    (state) => state.dataArrays.reformedMarkdownArr
+  );
 
-  console.log('document reformed', reformedMarkdownArr)
+  console.log("document reformed", reformedMarkdownArr);
 
-  const newDocArg = {key: newDocName, data: {
-    dateCreated: new Date(),
-    markup: [] as string[],
-    markdown: [] as string[]}}
+  const newDocArg = {
+    key: newDocName,
+    data: {
+      dateCreated: new Date(),
+      markup: [] as string[],
+      markdown: [] as string[],
+    },
+  };
 
+  const textInputRef = useRef<HTMLInputElement>(null);
 
-  const textInputRef = useRef<HTMLInputElement>(null)
-  
   // useEffect(() => {
   //   nameInput = document.querySelector('input.new-name') as HTMLInputElement
   // }, [])
 
   const handleCreateNewDoc = () => {
-    dispatch(closeButtonClicked(false))
-    setHideNewName(false)
+    dispatch(closeButtonClicked(false));
+    setHideNewName(false);
 
-    setTimeout(()=>{
-      textInputRef.current?.focus()
-    }, 500)
-    
-  }
+    setTimeout(() => {
+      textInputRef.current?.focus();
+    }, 500);
+  };
 
-  const handleNameSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    value && dispatch(addNewDoc(newDocArg))
-    setHideNewName(true)
-    setValue('')
-  }
+  const handleNameSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    value && dispatch(addNewDoc(newDocArg));
+    setHideNewName(true);
+    setValue("");
+  };
 
-  const addToTabBar = (listItem:string) => {
-    dispatch(updateTab(listItem))
-    dispatch(updateMarkdown(documents[listItem].markdown))
-  }
+  const addToTabBar = (listItem: string) => {
+    dispatch(updateTab(listItem));
+    dispatch(updateMarkdown(documents[listItem].markdown));
+  };
 
-  const updateRenderedMarkup = (docName:string, savedDocMarkup:string[]) => {
-    addToTabBar(docName)
-    dispatch(updateMarkup(savedDocMarkup))
-  }
+  const updateRenderedMarkup = (docName: string, savedDocMarkup: string[]) => {
+    addToTabBar(docName);
+    dispatch(updateMarkup(savedDocMarkup));
+  };
 
   // convertContent(reformedMarkdownArr)
 
-  const currentDocName = useAppSelector(state => state.header.currentDocName)
-  const docList = Array.from(Object.keys(documents)).reverse().map((item) => {
-    const date = documents[item].dateCreated.toString().slice(0,10)
-    return <li key={item} className='doc' onClick={() => {
-        updateRenderedMarkup(item, documents[item].markup)
-      }}>
-      <div className='icon-centered dark-box'>{documentIcon}</div>
-      <div className='doc-details'>
-        <p className='name-title'>{date}</p>
-        <p className='doc-name'>{item}.md</p>
-      </div>
-    </li>
-   })
+  const currentDocName = useAppSelector((state) => state.header.currentDocName);
+  const docList = Array.from(Object.keys(documents))
+    .reverse()
+    .map((item) => {
+      const date = documents[item].dateCreated.toString().slice(0, 10);
+      return (
+        <li
+          key={item}
+          className="doc"
+          onClick={() => {
+            // const currItem = documents[item].markup;
+            updateRenderedMarkup(item, documents[item].markup);
+          }}
+        >
+          <div className="icon-centered dark-box">{documentIcon}</div>
+          <div className="doc-details">
+            <p className="name-title">{date}</p>
+            <p className="doc-name">{item}.md</p>
+          </div>
+        </li>
+      );
+    });
 
-  const showMenu = useAppSelector(state => state.header.showMenu)
-  const menuClosed = useAppSelector(state => state.header.menuClosed)
-  const darkMode = useAppSelector(state => state.theme.darkMode)
-  const modalShowUpdate = useAppSelector(state => state.header.showModal)
+  const showMenu = useAppSelector((state) => state.header.showMenu);
+  const menuClosed = useAppSelector((state) => state.header.menuClosed);
+  const darkMode = useAppSelector((state) => state.theme.darkMode);
+  const modalShowUpdate = useAppSelector((state) => state.header.showModal);
 
-  if(!showMenu) return null
+  if (!showMenu) return null;
 
   return (
-      <div className={`sidebar flexed ${ modalShowUpdate ? 'faint' : '' }`}>
-        <div className='document-container flexed'>
-          <h6 className=' header document-header'>My Documents</h6>
-          <button className='btn new-doc-btn' onClick={handleCreateNewDoc}>+ New <span className='btn-span'>Document</span></button>
-          {/* <button className='btn new-doc-btn small-screen' onClick={handleCreateNewDoc}>+ New</button> */}
-          <form onSubmit={handleNameSubmit}>
-              <input ref = {textInputRef} className= {`new-name ${ hideNewName || menuClosed ? 'hidden' : ''}`} 
-              type='text' value={value}
-              placeholder='Type name here and press Enter'
-              onChange={(e) => setValue(e.target.value)} />
-          </form>
-          
-          <div className='documents'>
-              <ul onClick={() => setHideNewName(true)} className='document-list flexed'>
-                {
-                  docList.map(listItem => (
-                    listItem
-                  ))
-                }
-              </ul>
-          </div>
-        </div>
+    <div className={`sidebar flexed ${modalShowUpdate ? "faint" : ""}`}>
+      <div className="document-container flexed">
+        <h6 className=" header document-header">My Documents</h6>
+        <button className="btn new-doc-btn" onClick={handleCreateNewDoc}>
+          + New <span className="btn-span">Document</span>
+        </button>
+        {/* <button className='btn new-doc-btn small-screen' onClick={handleCreateNewDoc}>+ New</button> */}
+        <form onSubmit={handleNameSubmit}>
+          <input
+            ref={textInputRef}
+            className={`new-name ${hideNewName || menuClosed ? "hidden" : ""}`}
+            type="text"
+            value={value}
+            placeholder="Type name here and press Enter"
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </form>
 
-        <div className='visual-mode flexed'>
-          <div><Moon color={ darkMode ? '#fff' : '#7c8187' } size={16} /></div>
-          <div onClick={() =>dispatch(activateDarkMode(!darkMode))} className='toggle-mode flexed'>{ darkMode ? toggleLeftIcon : toggleRightIcon}</div>
-          <div><Sun color={ darkMode ? '#7c8187' : '#fff' }size={16} /></div>
+        <div className="documents">
+          <ul
+            onClick={() => setHideNewName(true)}
+            className="document-list flexed"
+          >
+            {docList.map((listItem) => listItem)}
+          </ul>
         </div>
       </div>
-    )
-  }
 
-export default DocumentSliceView
+      <div className="visual-mode flexed">
+        <div>
+          <Moon color={darkMode ? "#fff" : "#7c8187"} size={16} />
+        </div>
+        <div
+          onClick={() => dispatch(activateDarkMode(!darkMode))}
+          className="toggle-mode flexed"
+        >
+          {darkMode ? toggleLeftIcon : toggleRightIcon}
+        </div>
+        <div>
+          <Sun color={darkMode ? "#7c8187" : "#fff"} size={16} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DocumentSliceView;
--- src/features/markdown/MarkdownSliceView.tsx
+++ src/features/markdown/MarkdownSliceView.tsx
@@ -1,63 +1,112 @@
-import React, { useRef, useEffect } from 'react'
-import './markdown.css'
-import { useAppDispatch, useAppSelector } from '../../app/hooks'
-import { convertContent } from '../../utils/convertContent'
-import { resetCurrMarkdown, setNewMarkdownArr, setReformedMarkdownArr } from '../dataArrays/dataArraysSlice'
-import { updateMarkup } from '../dataArrays/dataArraysSlice'
+import React, { useRef, useEffect, useState } from "react";
+import "./markdown.css";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { convertContent } from "../../utils/convertContent";
+import {
+  resetCurrMarkdown,
+  setNewMarkdownArr,
+  setReformedMarkdownArr,
+} from "../dataArrays/dataArraysSlice";
+import { updateMarkup } from "../dataArrays/dataArraysSlice";
 
 // export let markdownArr: string[] = []
 // export let sortedMarkdownArr: string[] = []
 
-
 const MarkdownSliceView = () => {
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+  const currDocName = useAppSelector((state) => state.header.currentDocName);
+  const currMarkdown = useAppSelector((state) =>
+    currDocName ? state.documents[currDocName].markdown : []
+  ).join("\n");
 
-  const markdownRef = useRef<HTMLTextAreaElement>(null)
-  const currDocName = useAppSelector(state => state.header.currentDocName)
-  const currMarkdown = useAppSelector((state) => currDocName ? state.documents[currDocName].markdown : []).join('\n')
-
-  const handleMarkdownUpdate = (event:React.ChangeEvent<HTMLTextAreaElement>): void => {
-    const newMarkdownArr = currMarkdown ===  markdownRef.current!.value ? currMarkdown.split('\n') : markdownRef.current!.value.split('\n')
-    const reformedMarkdownArr = newMarkdownArr
-                .reduce<string[]>((prev: string[], curr:string, i:number, array:string[]): string[] => {
-                  if(!prev.length) prev.push(curr)
-                  else if(array[i-1].slice(0,1) === '<' && curr.slice(0,3) === '```') prev[prev.length-1] += curr
-                  else if((array[i-1].slice(0,3) === '```' && curr.slice(0,1) === '<') || curr.slice(0,1) === '<' || (/^ +</.test(curr.slice()))) prev[prev.length-1] += curr
-                  else if(array[i-1].slice(0,1) !== '-' && array[i-1].slice(1,3) !== '. ') prev.push(curr)
-                  else if( (array[i-1].slice(1,3) === '. ' || curr.slice(1,3) === '. ') && (array[i-1].slice(1,3)  !== curr.slice(1,3))) prev.push(curr)
-                  else if(array[i-1].slice(0,1) === '-' && curr.slice(0,1) === '-') prev[prev.length-1] += curr
-                  else if(array[i-1].slice(1,3) === '. ' && curr.slice(1,3) === '. '){
-                    prev[prev.length-1] = prev[prev.length-1] + '_' + curr.slice(3) }
-                  else prev.push(curr)
-                  return prev
-                }, [])
-    dispatch(setReformedMarkdownArr(reformedMarkdownArr))
-    dispatch(setNewMarkdownArr(newMarkdownArr))
+  const handleMarkdownUpdate = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ): void => {
+    const newMarkdownArr =
+      currMarkdown === markdownRef?.current?.value
+        ? currMarkdown.split("\n")
+        : markdownRef.current!.value.split("\n");
+    const reformedMarkdownArr = newMarkdownArr.reduce<string[]>(
+      (prev: string[], curr: string, i: number, array: string[]): string[] => {
+        if (!prev.length) prev.push(curr);
+        else if (array[i - 1].slice(0, 1) === "<" && curr.slice(0, 3) === "```")
+          prev[prev.length - 1] += curr;
+        else if (
+          (array[i - 1].slice(0, 3) === "```" && curr.slice(0, 1) === "<") ||
+          curr.slice(0, 1) === "<" ||
+          /^ +</.test(curr.slice())
+        )
+          prev[prev.length - 1] += curr;
+        else if (
+          array[i - 1].slice(0, 1) !== "-" &&
+          array[i - 1].slice(1, 3) !== ". "
+        )
+          prev.push(curr);
+        else if (
+          (array[i - 1].slice(1, 3) === ". " || curr.slice(1, 3) === ". ") &&
+          array[i - 1].slice(1, 3) !== curr.slice(1, 3)
+        )
+          prev.push(curr);
+        else if (array[i - 1].slice(0, 1) === "-" && curr.slice(0, 1) === "-")
+          prev[prev.length - 1] += curr;
+        else if (
+          array[i - 1].slice(1, 3) === ". " &&
+          curr.slice(1, 3) === ". "
+        ) {
+          prev[prev.length - 1] = prev[prev.length - 1] + "_" + curr.slice(3);
+        } else prev.push(curr);
+        return prev;
+      },
+      []
+    );
+    dispatch(setReformedMarkdownArr(reformedMarkdownArr));
+    dispatch(setNewMarkdownArr(newMarkdownArr));
 
     // console.log('markdown ',markdownArr)
-   
-}
+  };
   useEffect(() => {
-    markdownRef.current!.value = currMarkdown
-  },[currMarkdown])
-  
+    const markdown = currMarkdown;
+    markdownRef.current!.value = markdown;
+  }, [currMarkdown]);
 
-  const dispatch = useAppDispatch()
-  const menuOpen = useAppSelector(state => state.header.showMenu)
-  const darkMode = useAppSelector(state => state.theme.darkMode)
-  const modalShowUpdate = useAppSelector(state => state.header.showModal)
-  const reformedMarkdownArr = useAppSelector(state => state.dataArrays.reformedMarkdownArr)
-  
+  const _handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const currReformedMardownArr = reformedMarkdownArr;
+    handleMarkdownUpdate(e);
+    dispatch(updateMarkup(convertContent(currReformedMardownArr)));
+  };
+
+  const dispatch = useAppDispatch();
+  const menuOpen = useAppSelector((state) => state.header.showMenu);
+  const darkMode = useAppSelector((state) => state.theme.darkMode);
+  const modalShowUpdate = useAppSelector((state) => state.header.showModal);
+  const reformedMarkdownArr = useAppSelector(
+    (state) => state.dataArrays.reformedMarkdownArr
+  );
+
+  useEffect(() => {
+    dispatch(updateMarkup(convertContent(reformedMarkdownArr)));
+  }, [reformedMarkdownArr, dispatch]);
+
   return (
-    <div className={`markdown grid ${ modalShowUpdate ? 'faint' : '' }`}>
-      <h6 className={`markdown-header left-indent ${darkMode ? 'dark-header' : ''}`}>Markdown</h6>
-      <div className={`markdown-container ${ darkMode ? 'dark-markdown' : ''}`}>
-        <textarea ref={markdownRef} className={`markdown-text ${darkMode ? 'dark-text' : ''}`} onChange={(event) => {
-        handleMarkdownUpdate(event)
-        dispatch(updateMarkup(convertContent(reformedMarkdownArr)))
-        }}></textarea>
+    <div className={`markdown grid ${modalShowUpdate ? "faint" : ""}`}>
+      <h6
+        className={`markdown-header left-indent ${
+          darkMode ? "dark-header" : ""
+        }`}
+      >
+        Markdown
+      </h6>
+      <div className={`markdown-container ${darkMode ? "dark-markdown" : ""}`}>
+        <textarea
+          ref={markdownRef}
+          className={`markdown-text ${darkMode ? "dark-text" : ""}`}
+          onChange={(event) => {
+            _handleChange(event);
+          }}
+        ></textarea>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MarkdownSliceView
+export default MarkdownSliceView;
